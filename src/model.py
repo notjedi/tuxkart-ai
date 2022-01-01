f608db3fbc5081131091c64f721542df61ec6987
Krithic Kumar <krithickumarub@protonmail.com>
2022-01-01 18:26:10 +0530

    feat: add MultiCategorical class for multiple categorical outputs, remove pi function and other minor changes

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+
 from torch import nn
 from torch.distributions import Bernoulli, Categorical
 from torch.nn import functional as F
@@ -33,7 +34,7 @@ class ConvBlock(nn.Module):
     W_out = ((W_in + 2P - (F - 1) - 1) / S) + 1 (assuming dilation=1)
 
     :param in_channels: Number of input channels
-    :param in_channels: Number of output channels
+    :param out_channels: Number of output channels
     :param kernel_size: Size of the kernel/filter
     :param padding: The size of the zero-padding
     :param stride: Value of stride
@@ -87,69 +88,78 @@ class ResBlock(nn.Module):
         return self.bn2(out)
 
 
+class MultiCategorical():
+
+    def __init__(self, action_shape):
+        self.action_shape = action_shape
+        self.dist = None
+
+    def update_logits(self, logits):
+        self.dist = [Categorical(logits=split) for split in torch.split(logits,
+            self.action_shape, dim=1)]
+        return self
+
+    def get_actions(self, logits=None, deterministic=False) -> torch.Tensor:
+
+        if logits is not None:
+            self.update_logits(logits)
+        if deterministic:
+            return self.mode()
+        return self.sample()
+
+    def sample(self) -> torch.Tensor:
+        assert self.dist is not None
+        return torch.stack([dist.sample() for dist in self.dist], dim=1)
+
+    def mode(self) -> torch.Tensor:
+        assert self.dist is not None
+        return torch.stack([torch.argmax(dist.probs) for dist in self.dist], dim=1)
+
+
 class Actor(nn.Module):
     """
     Actor head for the PPO class to determine the best policy and action distribution.
     References:
         1. https://discuss.pytorch.org/t/multidmensional-actions/88259
 
-    :param inputDims: The shape of the input image (W, H, C)
-    :param numLogits: The number of logits in the last layer of the network
+    :param obs_shape: The shape of the input image (W, H, C)
+    :param action_shape: The shape of the action space Eg: `MultiDiscrete().nvec`
     """
 
-    def __init__(self, inputDims, numLogits):
+    def __init__(self, obs_shape, action_shape):
         super(Actor, self).__init__()
 
-        self.inputDims = inputDims
-        self.numLogits = numLogits
-        self.numActions = numLogits - 2
+        self.obs_shape = obs_shape
+        self.action_shape = action_shape
         self.reshape = FCView()
         self.actor1 = ConvBlock(in_channels=256, out_channels=128,
                 kernel_size=3, padding=1, stride=1)
         self.actor2 = ConvBlock(in_channels=128, out_channels=32,
                 kernel_size=3, padding=1, stride=1)
-        self.fc_actor = nn.Linear(np.prod((32,) + self.inputDims), self.numLogits)
+        self.fc_actor = nn.Linear(np.prod((32,) + self.obs_shape), np.sum(self.action_shape))
+        self.dist = MultiCategorical(action_shape)
 
-    def __call__(self, x, deterministic=False):
-        return self.pi(x, deterministic)
-
-    def forward(self, x):
+    def forward(self, x, deterministic=False):
         x = F.relu(self.actor1(x))
         x = self.actor2(x)
         x = self.fc_actor(self.reshape(x))
-        return x
-
-    def pi(self, x, deterministic=False):
-        x = self.forward(x)
-        steerProb = F.softmax(x[:, :3], dim=1)
-        actionProb = torch.sigmoid(x[:, 3:])
-        if deterministic:
-            steer = torch.argmax(steerProb, dim=1)
-            actions = torch.round(actionProb)
-            pi = torch.cat((steer.view(-1, 1), actions), dim=1)
-            return pi
-        else:
-            # https://mathworld.wolfram.com/BernoulliDistribution.html
-            # https://stats.stackexchange.com/a/113381
-            steerDist = Categorical(steerProb)
-            actionDist = Bernoulli(actionProb)
-            return steerDist, actionDist
+        return self.dist.update_logits(logits=x)
 
 
 class Critic(nn.Module):
     """
     Critic head for the PPO class to determine the expected rewards.
 
-    :param inputDims: The shape of the input image (W, H, C)
+    :param obs_shape: The shape of the input image (W, H, C)
     """
 
-    def __init__(self, inputDims):
+    def __init__(self, obs_shape):
         super(Critic, self).__init__()
 
-        self.inputDims = inputDims
+        self.obs_shape = obs_shape
         self.reshape = FCView()
         self.critic= ConvBlock(in_channels=256, out_channels=32, kernel_size=3, padding=1, stride=1)
-        self.fc_critic = nn.Linear(np.prod((32,) + self.inputDims), 1)
+        self.fc_critic = nn.Linear(np.prod((32,) + self.obs_shape), 1)
 
     def forward(self, x):
         x = F.relu(self.critic(x))
@@ -157,7 +167,7 @@ class Critic(nn.Module):
         return x
 
 
-class PPO(nn.Module):
+class Net(nn.Module):
     """
     Proximal Policy Optimization algorithm (PPO)
 
@@ -165,20 +175,18 @@ class PPO(nn.Module):
     Introduction to PPO: https://spinningup.openai.com/en/latest/algorithms/ppo.html
     Implementation: https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/ppo/ppo.py
 
-    :param inputDims: The shape of the input image (W, H, C)
-    :param numLogits: The number of logits in the last layer of the network
+    :param obs_shape: The shape of the input image (W, H, C)
+    :param action_shape: The shape of the action space Eg: `MultiDiscrete().nvec`
     """
 
-    def __init__(self, inputDims, numLogits):
-        super(PPO, self).__init__()
+    def __init__(self, obs_shape: tuple, action_shape: tuple, num_frames: int):
+        super(Net, self).__init__()
 
-        self.timeDim = 5
-        self.numResBlocks = 10
-        self.downSampleLayer = (self.numResBlocks // 2)
-        # channel-first layout
-        self.inputDims = tuple(reversed(inputDims))
+        self.num_res_blocks = 1
+        self.downSampleLayer = (self.num_res_blocks // 2)
+        self.obs_shape = obs_shape
 
-        self.conv1 = ConvBlock(in_channels=self.inputDims[0], out_channels=256,
+        self.conv1 = ConvBlock(in_channels=self.obs_shape[-1], out_channels=256,
                 kernel_size=3, padding=1, stride=1)
         for block in range(0, self.downSampleLayer):
             setattr(self, f"res-block-{block+1}", ResBlock(in_channels=256,
@@ -187,14 +195,20 @@ class PPO(nn.Module):
         self.conv2 = ConvBlock(in_channels=256, out_channels=256,
                 kernel_size=5, padding=1, stride=1)
 
-        for block in range(self.downSampleLayer, self.numResBlocks):
+        for block in range(self.downSampleLayer, self.num_res_blocks):
             setattr(self, f"res-block-{block+1}", ResBlock(in_channels=256,
                 out_channels=256, kernel_size=3, padding=1, stride=1))
 
-        inputDims = tuple(map(lambda x: (x + 2 - 4 - 1)//1 + 1,
-            (self.timeDim,) + self.inputDims[1:]))
-        self.actor = Actor(inputDims, numLogits)
-        self.critic = Critic(inputDims)
+        # change input dimensions for the Actor and Critic block
+        # as it is downscaled by previous resnet layers
+        # W_out = ((W_in + 2P - (F - 1) - 1) / S) + 1 (assuming dilation=1)
+        # Refer: https://pytorch.org/docs/1.9.1/generated/torch.nn.Conv3d.html
+        # TODO: make this dynamic
+        obs_shape = (num_frames, ) + self.obs_shape[:-1]
+        obs_shape = tuple(map(lambda x: (x + 2 - 4 - 1)//1 + 1, obs_shape))
+
+        self.actor = Actor(obs_shape, action_shape)
+        self.critic = Critic(obs_shape)
         self._initialize_weights()
 
     def _initialize_weights(self):
@@ -208,14 +222,34 @@ class PPO(nn.Module):
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
 
-    def forward(self, x, deterministic=False):
-        x = self.conv1(x)
+    def _forward_base(self, obs: torch.Tensor):
+        obs = self.conv1(obs)
         for block in range(0, self.downSampleLayer):
-            x = getattr(self, f"res-block-{block+1}")(x)
+            obs = getattr(self, f"res-block-{block+1}")(obs)
 
-        x = self.conv2(x)
-        for block in range(self.downSampleLayer, self.numResBlocks):
-            x = getattr(self, f"res-block-{block+1}")(x)
-        policy = self.actor(x, deterministic)
-        value = self.critic(x)
+        obs = self.conv2(obs)
+        for block in range(self.downSampleLayer, self.num_res_blocks):
+            obs = getattr(self, f"res-block-{block+1}")(obs)
+        return obs
+
+    def forward(self, obs: torch.Tensor, deterministic: bool = False):
+        obs = self._forward_base(obs)
+        policy = self.actor(obs, deterministic)
+        value = self.critic(obs)
         return policy, value
+
+if __name__ == '__main__':
+
+    DEVICE = 'cuda'
+    BATCH_SIZE = 8
+    NUM_FRAMES = 5
+    OBS_DIM = (60, 40, 3)
+    ACT_DIM = (2, 2, 3, 2, 2, 2, 2)
+
+    randInput = torch.randint(0, 10, (BATCH_SIZE, OBS_DIM[-1], NUM_FRAMES, *OBS_DIM[:-1]),
+            device=DEVICE, dtype=torch.float16)
+    model = Net(OBS_DIM, ACT_DIM, NUM_FRAMES)
+    model.to(DEVICE)
+
+    policy, value = model(randInput)
+    print(policy.sample(), value, sep='\n')
