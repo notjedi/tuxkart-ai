9f80d76d28f287cee9869dc84634054bce86f01d
Krithic Kumar <krithickumarub@protonmail.com>
2022-02-02 14:34:18 +0530

    fix: modifications to fit the changs in LSTM class

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ from src.vae.model import ConvVAE, Encoder, Decoder
 def eval(vae, lstm, logger, args):
 
     from eval import eval
+
     try:
         env = SubprocVecEnv([make_env(id) for id in range(1)], start_method='spawn')
         tot_reward = np.sum(eval(env, vae, lstm, logger, args)) / args.num_envs
@@ -52,18 +53,26 @@ def main(args):
         print(f"loading LSTM model from {args.lstm_model_path}")
         lstm.load_state_dict(torch.load(args.lstm_model_path))
 
-    race_config_args = { 'track': args.track, 'kart': args.kart }
+    race_config_args = {'track': args.track, 'kart': args.kart}
     prev_reward, curr_reward = -float('inf'), 0
     optimizer = optim.Adam(lstm.parameters(), lr=args.lr, eps=1e-5)
     writer = SummaryWriter(log_dir=args.log_dir)
     logger = Logger(writer)
 
     for i in trange(args.num_global_steps):
-        env = SubprocVecEnv([make_env(id, args.graphic, race_config_args) for id in
-            range(args.num_envs)], start_method='spawn')
-        buf_args = { 'buffer_size': args.buffer_size, 'batch_size': args.num_envs, 'zdim':
-                vae.zdim + 4, 'act_dim': env.action_space.nvec, 'num_frames':
-                args.num_frames }
+        torch.cuda.empty_cache()
+        env = SubprocVecEnv(
+            [make_env(id, args.graphic, race_config_args) for id in range(args.num_envs)],
+            start_method='spawn',
+        )
+        buf_args = {
+            'buffer_size': args.buffer_size,
+            'batch_size': args.num_envs,
+            'zdim': vae.zdim + 4,
+            'act_dim': env.action_space.nvec,
+            'num_frames': args.num_frames,
+        }
+        lstm.reset(args.buffer_size, args.num_envs)
         ppo = PPO(env, vae, lstm, optimizer, logger, args.device, **buf_args)
 
         try:
@@ -78,29 +87,36 @@ def main(args):
             env.close()
             print("Exiting...")
 
-        if (i % args.eval_interval == 0 and i != 0):
+        if i % args.eval_interval == 0 and i != 0:
             curr_reward = eval(vae, lstm, logger, args)
             print(curr_reward)
-            vae.train()
-            lstm.train()
-            if (curr_reward > prev_reward):
-                print(f'{curr_reward} is better than {prev_reward}, \
-                        saving model to path model/lstm-{i}.pth')
+            if curr_reward > prev_reward:
+                print(
+                    f'{curr_reward} is better than {prev_reward}, \
+                        saving model to path model/lstm-{i}.pth'
+                )
                 prev_reward = curr_reward
                 torch.save(lstm.state_dict(), f'{args.save_dir}/lstm-{i}.pth')
 
 
 if __name__ == '__main__':
     from os.path import join
-    parser = argparse.ArgumentParser("Implementation of the PPO algorithm for the SuperTuxKart game")
+
+    parser = argparse.ArgumentParser(
+        "Implementation of the PPO algorithm for the SuperTuxKart game"
+    )
     # env arguments
     parser.add_argument('--kart', type=str, choices=STK.KARTS, default=None)
     parser.add_argument('--track', type=str, choices=STK.TRACKS, default=None)
     parser.add_argument('--graphic', type=str, choices=['hd', 'ld', 'sd'], default='hd')
 
     # model args
-    parser.add_argument('--vae_model_path', type=Path, default=None, help='Load VAE model from path.')
-    parser.add_argument('--lstm_model_path', type=Path, default=None, help='Load LSTM model from path.')
+    parser.add_argument(
+        '--vae_model_path', type=Path, default=None, help='Load VAE model from path.'
+    )
+    parser.add_argument(
+        '--lstm_model_path', type=Path, default=None, help='Load LSTM model from path.'
+    )
     parser.add_argument('--zdim', type=int, default=256)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--seed', type=int, default=1337)
@@ -108,15 +124,23 @@ if __name__ == '__main__':
     parser.add_argument('--buffer_size', type=int, default=512)
 
     # train args
-    parser.add_argument('--num_envs', type=int, default=2)
+    parser.add_argument('--num_envs', type=int, default=5)
     parser.add_argument('--eval_steps', type=int, default=512)
     parser.add_argument('--eval_interval', type=int, default=10)
     parser.add_argument('--num_global_steps', type=int, default=5000)
     parser.add_argument('--device', type=str, choices=['cpu', 'cuda'], default='cuda')
-    parser.add_argument('--log_dir', type=Path, default=join(Path(__file__).absolute().parent,
-    'tensorboard'), help='Path to the directory in which the tensorboard logs are saved.')
-    parser.add_argument('--save_dir', type=Path, default=join(Path(__file__).absolute().parent,
-        'models'), help='Path to the directory in which the trained models are saved.')
+    parser.add_argument(
+        '--log_dir',
+        type=Path,
+        default=join(Path(__file__).absolute().parent, 'tensorboard'),
+        help='Path to the directory in which the tensorboard logs are saved.',
+    )
+    parser.add_argument(
+        '--save_dir',
+        type=Path,
+        default=join(Path(__file__).absolute().parent, 'models'),
+        help='Path to the directory in which the trained models are saved.',
+    )
     args = parser.parse_args()
 
     main(args)
