f41db899a7ec2096f883fa18f6e750f21ea036ef
Krithic Kumar <krithickumarub@protonmail.com>
2024-06-15 19:11:36 +0530

    refactor: training code and architecture

--- train_vae.py
+++ train_vae.py
@@ -10,8 +10,8 @@ import numpy.typing as npt
 import pystk
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
 from PIL import Image
 from pystk_gym.common.race import RaceConfig
 from torch.utils.data import DataLoader, Dataset
@@ -20,62 +20,97 @@ from torchvision import transforms
 from tqdm import tqdm
 
 SAMPLE_RATE = 0.9
+CLASS_COLOR = (
+    (
+        # https://pystk.readthedocs.io/en/latest/data.html#pystk.ObjectType
+        np.array(
+            [
+                0x000000,  # None
+                0x4E9A06,  # Kart
+                0x2E3436,  # Track
+                0xEEEEEC,  # Background
+                0x204A87,  # Pickup
+                0x204A87,  # Nitro
+                0xA40000,  # Bomb
+                0xCE5C00,  # Object
+                0x5C3566,  # Projectile
+                0x000000,  # Unknown
+                0x000000,  # N
+            ],
+            dtype=">u4",
+        )
+        .view(np.uint8)
+        .reshape((-1, 4))[:, 1:]
+    )
+    .flatten()
+    .tolist()
+)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        residual = x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += residual
+        out = F.relu(out)
+        return out
 
 
 class VQVAE(nn.Module):
-    def __init__(self, num_embeddings=1024, embedding_dim=2048):
+    def __init__(self, num_embeddings=512, embedding_dim=2048):
         super(VQVAE, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 128, 4, stride=2, padding=1),
+            nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 128, 4, stride=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, embedding_dim, 4, stride=2, padding=1),
+            nn.Conv2d(128, embedding_dim, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            ResidualBlock(embedding_dim, embedding_dim),
+            # ResidualBlock(embedding_dim, embedding_dim),
         )
-        self.codebook = nn.Embedding(num_embeddings, embedding_dim)
-        self.codebook.weight.data.uniform_(-1 / num_embeddings, 1 / num_embeddings)
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(embedding_dim, 128, 4, stride=2, padding=1),
+            # ResidualBlock(embedding_dim, embedding_dim),
+            ResidualBlock(embedding_dim, embedding_dim),
+            nn.ConvTranspose2d(embedding_dim, 128, kernel_size=5, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 128, 4, stride=2, padding=1),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 1, 4, stride=2, padding=1),
-            nn.Sigmoid(),
+            nn.ConvTranspose2d(64, 1, kernel_size=3, stride=2, padding=1),
         )
 
-    def encode(self, x):
-        z_e = self.encoder(x)
-        return z_e
-
-    def decode(self, z_q):
-        x_recon = self.decoder(z_q)
-        return x_recon
+        self.embeddings = nn.Embedding(num_embeddings, embedding_dim)
+        self.embeddings.weight.data.uniform_(-1 / num_embeddings, 1 / num_embeddings)
 
     def forward(self, x):
-        z_e = self.encode(x)
+        z_e = self.encoder(x)
         z_e_flattened = (
-            z_e.view(z_e.shape[0], z_e.shape[1], -1).permute(0, 2, 1).contiguous()
+            z_e.permute(0, 2, 3, 1).contiguous().view(-1, self.embedding_dim)
         )
+
         distances = (
-            (z_e_flattened**2).sum(dim=2, keepdim=True)
-            + (self.codebook.weight**2).sum(dim=1)
-            - 2 * (z_e_flattened @ self.codebook.weight.t())
+            torch.sum(z_e_flattened**2, dim=1, keepdim=True)
+            + torch.sum(self.embeddings.weight**2, dim=1)
+            - 2 * (z_e_flattened @ self.embeddings.weight.t())
         )
-        encoding_indices = distances.argmin(dim=2)
-        z_q = (
-            self.codebook(encoding_indices)
-            .permute(0, 2, 1)
-            .contiguous()
-            .view(z_e.shape)
-        )
-        x_recon = self.decode(z_q)
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        z_q = self.embeddings(encoding_indices).view(z_e.size())
 
-        commitment_loss = 0.25 * torch.mean((z_e.detach() - z_q) ** 2)
-        quantization_loss = torch.mean((z_e - z_q.detach()) ** 2)
-        loss = commitment_loss + quantization_loss
-
-        return x_recon, loss
+        return self.decoder(z_q), z_e, z_q
 
 
 class CustomImageDataset(Dataset):
@@ -87,24 +122,53 @@ class CustomImageDataset(Dataset):
         return len(self.image_datas)
 
     def __getitem__(self, idx):
-        # TODO: process and split the data here
         image = self.image_datas[idx]
         if self.transform:
             image = self.transform(image)
         return image
 
 
+def cmap_semantic_image(img: Image.Image) -> np.ndarray:
+    img.putpalette(CLASS_COLOR)
+    return np.array(img.convert("L"))
+
+
+def get_pystk_configs() -> Tuple[pystk.GraphicsConfig, pystk.RaceConfig]:
+    graphic_config = pystk.GraphicsConfig.hd()
+    graphic_config.screen_width = 960
+    graphic_config.screen_height = 540
+
+    num_players = 4
+    race_config = pystk.RaceConfig()
+    race_config.laps = 1
+    race_config.num_kart = num_players
+    race_config.players[0].kart = np.random.choice(RaceConfig.KARTS)
+    race_config.players[0].controller = pystk.PlayerConfig.Controller.AI_CONTROL
+
+    for _ in range(1, num_players):
+        race_config.players.append(
+            pystk.PlayerConfig(
+                np.random.choice(RaceConfig.KARTS),
+                pystk.PlayerConfig.Controller.AI_CONTROL,
+                0,
+            )
+        )
+    race_config.track = np.random.choice(RaceConfig.TRACKS)
+    race_config.step_size = 0.345
+
+    return (graphic_config, race_config)
+
+
 def generate_images(
     graphic_config: pystk.GraphicsConfig,
     race_config: pystk.RaceConfig,
     sample_rate: float,
+    max_samples: int = 64,
 ) -> npt.NDArray[np.float32]:
-    datas = []
-    total_samples = 64
+    datas, samples = [], 0
     race, state, steps, t0 = None, None, 0, 0
-    pbar = tqdm(total=total_samples)
 
-    while pbar.n < total_samples:
+    while samples < max_samples:
         if (race is None or state is None) or any(
             kart.finish_time > 0 for kart in state.karts
         ):
@@ -126,17 +190,20 @@ def generate_images(
         race.step()
         state.update()
 
-        for kart_render_data in race.render_data:
-            if random.random() < sample_rate:
+        if random.random() < sample_rate:
+            # TODO: replace with num_players from config
+            samples += race_config.num_kart
+            for kart_render_data in race.render_data:
                 img = np.array(
                     Image.fromarray(kart_render_data.image).convert("L")
                 ) / np.float32(255.0)
-                depth = kart_render_data.depth
-                instance = (kart_render_data.instance & 0xFFFFFF).astype(np.float32)
-                # semantic = (kart_render_data.instance >> 24) & 0xFF
-                data = np.dstack((img, depth, instance))
+                depth = kart_render_data.depth.astype(np.float32)
+                semantic = (kart_render_data.instance >> 24) & 0xFF
+                semantic = cmap_semantic_image(
+                    Image.fromarray(semantic.astype(np.uint8))
+                ) / np.float32(255.0)
+                data = np.stack((img, depth, semantic))
                 datas.append(data)
-                pbar.update(1)
 
         steps += 1
         delta_d = steps * race_config.step_size - (time.time() - t0)
@@ -150,72 +217,74 @@ def generate_images(
     return np.array(datas)
 
 
-def get_pystk_configs() -> Tuple[pystk.GraphicsConfig, pystk.RaceConfig]:
-    graphic_config = pystk.GraphicsConfig.hd()
-    graphic_config.screen_width = 960
-    graphic_config.screen_height = 540
-
-    num_players = 5
-    race_config = pystk.RaceConfig()
-    race_config.laps = 1
-    race_config.num_kart = num_players
-    race_config.players[0].kart = np.random.choice(RaceConfig.KARTS)
-    race_config.players[0].controller = pystk.PlayerConfig.Controller.AI_CONTROL
-
-    for _ in range(1, num_players):
-        race_config.players.append(
-            pystk.PlayerConfig(
-                np.random.choice(RaceConfig.KARTS),
-                pystk.PlayerConfig.Controller.AI_CONTROL,
-                0,
-            )
-        )
-    race_config.track = np.random.choice(RaceConfig.TRACKS)
-    race_config.step_size = 0.345
-
-    return (graphic_config, race_config)
-
-
 def main(args):
     graphic_config, race_config = get_pystk_configs()
-    datas = generate_images(graphic_config, race_config, SAMPLE_RATE)
-    print(datas.shape)
-
-    device = args.device
-    if device == "cuda":
-        assert torch.cuda.is_available(), "cuda is not is_available"
+    datas = generate_images(graphic_config, race_config, SAMPLE_RATE, 16)
 
     # TODO: should i mmap the data and read it later?
-    transform = transforms.Compose([transforms.ToTensor()])
-    dataset = CustomImageDataset(datas, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+    # transform = transforms.Compose([transforms.ToTensor()])
+    dataset = CustomImageDataset(datas, transform=None)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True, num_workers=1)
+    dataloader_len = len(dataloader)
 
     tensorboard_file_name = args.log_dir.joinpath(f"vae/{args.zdim}-{args.loss_fn}/")
-    logger = SummaryWriter(tensorboard_file_name, flush_secs=60)
+    logger = SummaryWriter(tensorboard_file_name, flush_secs=30)
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = VQVAE().to(device)
+    # model = model.half()
+    # model = torch.compile(model)
+    # traced_model = torch.jit.trace(model, torch.from_numpy(datas[:2]).to("cuda"))
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.MSELoss()
 
-    num_epochs = 10
+    epochs = 20
+    epoch_progress_bar = tqdm(range(epochs), position=0, desc="Loss: inf")
 
-    for epoch in tqdm(range(num_epochs)):
+    for epoch in epoch_progress_bar:
         model.train()
         train_loss = 0.0
+        dataloader_progress_bar = tqdm(
+            dataloader, position=1, leave=False, desc="Loss: inf"
+        )
 
-        for images in tqdm(dataloader):
+        for batch_idx, images in enumerate(dataloader_progress_bar):
             images = images.to(device)
-            optimizer.zero_grad()
-            recon_images, loss = model(images)
+            # images = images.half()
+            grayscale_images = images[:, 0, :, :]
+            # img = transforms.functional.to_pil_image(
+            #     grayscale_images.float().cpu().detach().numpy()[0]
+            # )
+            # img.show()
+
+            # optimizer.zero_grad()
+            for param in model.parameters():
+                param.grad = None
+
+            outputs, z_e, z_q = model(images)
+            recons = F.interpolate(outputs, (540, 960), mode="nearest").squeeze(dim=1)
+            reconstruction_loss = criterion(recons, grayscale_images)
+            commitment_loss = torch.mean((z_e - z_q.detach()) ** 2)
+            vq_loss = torch.mean((z_q - z_e.detach()) ** 2)
+            loss = reconstruction_loss + commitment_loss + vq_loss
+
             loss.backward()
             optimizer.step()
-            train_loss += loss.item()
+            # torch.cuda.empty_cache()
+            loss_cpu = loss.item()
+            train_loss += loss_cpu
+            logger.add_scalar(
+                "Loss/Batch", loss_cpu, epoch * dataloader_len + batch_idx
+            )
+            dataloader_progress_bar.set_description(f"Loss: {loss_cpu:.6f}")
 
-        train_loss /= len(dataloader)
+        train_loss /= dataloader_len
+        logger.add_scalar("Loss/Epoch", train_loss, epoch)
+        epoch_progress_bar.set_description(f"Loss: {train_loss:.6f}")
         # model.eval()
         # decoded = model.decode(model.encode(images))
         # Image.fromarray(decoded.cpu().detach().numpy()[0].astype(np.uint8).transpose(1, 2, 0) * 255).show()
-        # torchvision.transforms.functional.to_pil_image(decoded.cpu().detach().numpy()[0].astype(np.uint8)).show()
-        print(f"Epoch {epoch + 1}, Loss: {train_loss}")
+        # transforms.functional.to_pil_image(decoded.cpu().detach().numpy()[0].astype(np.uint8)).show()
 
 
 if __name__ == "__main__":
